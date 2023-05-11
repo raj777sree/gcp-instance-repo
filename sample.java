2211fcfb89a88b304bcffac93e5c095eb8e5fac2
raj777sree <rajsreenath90@sample-instance1.us-west4-b.c.radiant-math-386105.internal>
2023-05-11 07:31:39 +0000

    this is a demo

---
+++ sample.java
@@ -0,0 +1,13 @@
+$ java ./Guess.java
+number is 38
+Guess a number between 1 and 100
+1
+Too low
+Guess a number between 1 and 100
+39
+Too high
+Guess a number between 1 and 100
+38
+That's right!
+$ 
+
